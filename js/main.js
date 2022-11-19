8f69c43685e26e26db7ef2fede4d1aa7180a91d1
Yoo Seungmin <dnjfht@naver.com>
2022-11-19 14:09:54 +0900

    renewal_teabless

---
+++ js/main.js
@@ -0,0 +1,332 @@
+$(document).ready(function(){
+
+    var swiper = new Swiper(".mySwiper1", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        slidesPerGroup: 5,
+        autoplay: true,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    });
+
+    var swiper = new Swiper(".mySwiper2", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        slidesPerGroup: 5,
+        autoplay: true,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    });
+
+    var swiper = new Swiper(".mySwiper3", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      slidesPerGroup: 5,
+      autoplay: true,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".mySwiper4", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerGroup: 5,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper5", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper6", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper7", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper8", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper9", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper10", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper11", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper12", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper13", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper14", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper15", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    autoplay: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "progressbar", //동그라미 버튼을 진행상황을 나타내는 바로 변경
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  // #gnb_wrap
+  $('#menu_bar').click(function(){
+    
+    $('#gnb_wrap').animate({
+        left:0
+    },1000);
+
+  });
+
+  $('#gnb_wrap #gnb_close').click(function(){
+    
+    $('#gnb_wrap').animate({
+        left:'-653px'
+    },1000);
+
+  });
+
+  i = 0;
+
+  $('#main>li:first-child>a').click(function(){
+  
+  if(i == 0) {
+    $('#gnb_wrap #depth_1,#gnb_wrap_8 #depth_1').animate({
+        height:'224px'
+    });
+    $('#main>li:first-child').addClass('on');
+    i++;
+  }else if(i == 1) {
+    $('#gnb_wrap #depth_1,#gnb_wrap_8 #depth_1').animate({
+        height:0
+    });
+    $('#main>li:first-child').removeClass('on');
+    i=0;
+  }
+
+  });
+
+
+
+  // #gnb_wrap_8
+  $('#menu_bar').click(function(){
+    
+    $('#gnb_wrap_8').animate({
+        left:0
+    },1000);
+
+  });
+
+  $('#gnb_wrap_8 #gnb_close').click(function(){
+    
+    $('#gnb_wrap_8').animate({
+        left:'-100vw'
+    },1000);
+
+  });
+
+
+
+});
